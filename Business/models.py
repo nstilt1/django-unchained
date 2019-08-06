47d49a7fb598aa44a14730e593ff682632fb4251
Noah Stiltner <nstilt1@lsu.edu>
2019-08-06 12:30:54 -0500

    employee name on BusinessEmployeeSerializer

--- Business/models.py
+++ Business/models.py
@@ -17,28 +17,22 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
-    business = models.ForeignKey(
-        'Business',
-        on_delete=models.CASCADE,
-        related_name='employees_of_company',
-        null=True,
-    )
     def __unicode__(self):
         return "%s, %s" % self.last_name, self.first_name
     def __str__(self):
         return "%s, %s" % (self.last_name, self.first_name)
 
 class BusinessEmployee(models.Model):
-    business = models.ForeignKey(
-        'Business',
+    business_of_employee = models.ForeignKey(
+        Business,
         on_delete=models.CASCADE,
-        related_name='business',
+        related_name='business_of_employee',
         null=True
     )
-    employee = models.ForeignKey(
+    employee_of_business = models.ForeignKey(
         'Employee',
         on_delete=models.CASCADE,
-        related_name='employee',
+        related_name='employee_of_business',
         null=True
     )
     is_owner = models.BooleanField(null=True)
